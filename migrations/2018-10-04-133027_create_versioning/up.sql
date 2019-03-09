9f5bd73498558d354769fa757ce1a6bd7912c00c
Atta Zadeh <atta.h.zadeh@gmail.com>
2019-03-09 13:43:00 -0500

    commiting refactored changes

--- migrations/2018-10-04-133027_create_versioning/up.sql
+++
@@ -1,7 +0,0 @@
--- keep track of versions
-
-CREATE TABLE version (
-    version_id            BIGSERIAL PRIMARY KEY,
-    version_update        VARCHAR NOT NULL UNIQUE,
-    updated_at            TIMESTAMP NOT NULL DEFAULT NOW()
-);
--- migrations/2018-10-08-150445_setup_admin/down.sql
+++
@@ -1,6 +0,0 @@
-
-ALTER TABLE user_account DROP CONSTRAINT user_account_admin_is_0_check;
-ALTER TABLE scope DROP CONSTRAINT scope_main_is_0_check;
-
-TRUNCATE user_account CASCADE;
-TRUNCATE scope CASCADE;
